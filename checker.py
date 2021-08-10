85693d147a8dc400cd87702158905981c0e1f7c1
hackettyu <hackett_yu@outlook.com>
2021-08-10 22:31:07 +0800

    Changed: timezone code

--- checker.py
+++ checker.py
@@ -1,23 +1,19 @@
 import datetime
 from chinese_calendar import is_workday, is_holiday, is_in_lieu
 
-today = datetime.date.today()
+# Fixed: timezone sub 16 hours cause UTC +8 in China
 one_day_duration = datetime.timedelta(days=1)
-tomorrow = today + one_day_duration
+today = datetime.date.today() - one_day_duration
+tomorrow = datetime.date.today()
 
 try:
-
     if tomorrow.weekday() in (5, 6) and is_workday(tomorrow):
-        print(f'Tomorrow {tomorrow} is workday and weekend, do you hava set the alarm clock?')
-    
+        print(f'Tomorrow {tomorrow} is workday and weekend, do you hava set the alarm clock?') # noqa
     elif is_workday(today):
         print(f'Just work in {today}.')
-
     elif is_in_lieu(today):
         print(f'Today {today} is holiday but also lieu.')
-
     elif is_holiday(today):
-        print(f'Today {today} is holiday, hava a good day!')
-
+        print(f'Today {today} is holiday, have a good day!')
 except NotImplementedError:
-    print('Please check chinese_calendar had updated and the date is invalid.')
+        print('Please check chinese_calendar had updated and the date is invalid.') # noqa
