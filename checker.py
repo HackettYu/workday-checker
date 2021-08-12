118c0587dd480132eeb668ad72034301fcb5a9d7
hackettyu <hackett_yu@outlook.com>
2021-08-12 23:17:22 +0800

    Added: sudo change timezone

--- checker.py
+++ checker.py
@@ -3,8 +3,8 @@ from chinese_calendar import is_workday, is_holiday, is_in_lieu
 
 # Fixed: timezone sub 16 hours cause UTC +8 in China
 one_day_duration = datetime.timedelta(days=1)
-today = datetime.date.today() - one_day_duration
-tomorrow = datetime.date.today()
+today = datetime.date.today()
+tomorrow = datetime.date.today() + one_day_duration
 
 try:
     if tomorrow.weekday() in (5, 6) and is_workday(tomorrow):
