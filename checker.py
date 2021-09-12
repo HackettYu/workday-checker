e92da5b61249465df0d92543689bbb1f43570a0f
hackettyu <hackett_yu@outlook.com>
2021-09-12 13:30:11 +0800

    Changed: git email

--- checker.py
+++ checker.py
@@ -8,15 +8,15 @@ one_day_duration = datetime.timedelta(days=1)
 def day_checker(pivot: datetime.date) -> None:
     try:
         if pivot.weekday() in (5, 6) and is_workday(pivot):
-            print(f'{pivot} is workday and weekend, do you have set the alarm clock?') # noqa
+            print(f'{pivot} is workday and weekend, do you have set the alarm clock?\n\r') # noqa
         elif is_workday(pivot):
-            print(f'Just work in {pivot}.')
+            print(f'Just work in {pivot}.\n\r')
         elif is_in_lieu(pivot):
-            print(f'{pivot} is holiday but also lieu.')
+            print(f'{pivot} is holiday but also lieu.\n\r')
         elif is_holiday(pivot):
-            print(f'{pivot} is holiday, have a good day!')
+            print(f'{pivot} is holiday, have a good day!\n\r')
     except NotImplementedError:
-            print('Please check chinese_calendar had updated and the date is invalid.') # noqa
+            print('Please check chinese_calendar had updated and the date is invalid.\n\r') # noqa
 
 
 if __name__ == "__main__":
